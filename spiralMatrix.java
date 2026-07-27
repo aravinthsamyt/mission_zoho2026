fe3614e003a5dd882b9377386cfbec7608cad443
aravinthsamyt <aravinthtamil2005@gmail.com>
2026-07-27 19:13:32 +0530

    spiral_matrix

---
+++ spiralMatrix.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class SpiralMatrix{
+    public static void main(String args[]){
+        int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        int left = 0,right=arr.length-1;
+        int top = 0,bottom=arr[0].length-1;
+        List <Integer> list = new ArrayList<>();
+        while(left<=right && top<=bottom){
+            for(int i=left;i<=right;i++){
+                list.add(arr[top][i]);
+            }
+            top++;
+            for(int i=top;i<=bottom;i++){
+                list.add(arr[i][right]);
+            }
+            right--;
+            if(top<=bottom){ 
+                for(int i=right;i>=left;i--){
+                    list.add(arr[bottom][i]);
+                }
+            bottom--;
+            }
+
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                list.add(arr[i][left]);
+                }
+                left++;
+            }
+        }
+        System.out.println(list);
+    }
+}
